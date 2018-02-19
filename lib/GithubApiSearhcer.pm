c75a1cec960a8b196f7a10267ce82da6d8190cc1
Morichan <nintendo698@gmail.com>
2018-02-19 08:50:56 +0000

    feat: GitHubのバージョン番号を取得できた

--- lib/GithubApiSearhcer.pm
+++ lib/GithubApiSearhcer.pm
@@ -6,27 +6,35 @@ use lib "/home/ubuntu/perl5/lib/perl5/x86_64-linux-gnu-thread-multi/";
 use Mouse;
 use JSON;
 
-has url => (is => "rw", default => "https://api.github.com/search/repositories?q=");
-has release_url => (is => "rw", default => "https://apt.github.com/repos/");
-has json_data => (is => "rw");
+has searching_url => (is => "rw", default => "https://api.github.com/search/repositories?q=");
+has release_url => (is => "rw", default => "https://api.github.com/repos/");
+has searching_json_data => (is => "rw");
+has release_json_data => (is => "rw");
+has latest_release_version => (is => "rw");
 
 sub search_github_api_and_name_eq {
     my $self = shift;
     my $name = shift;
 
-    $self->add_url_and_name($name);
-    $self->format_json_from_github_apt($self->url);
-    my $full_name = $self->json_data->{items}[0]{full_name};
-    $self->release_url($self->release_url . $full_name);
+    $self->add_searching_url_and_name($name);
+    $self->searching_json_data(
+        $self->format_json_from_github_apt($self->searching_url));
+    my $full_name = $self->searching_json_data->{items}[0]{full_name};
 
-    return ;
+    $self->release_url($self->release_url . $full_name . "/releases");
+    $self->release_json_data(
+        $self->format_json_from_github_apt($self->release_url));
+    $self->latest_release_version(
+        $self->release_json_data->[0]{name});
+
+    return $self->latest_release_version;
 }
 
-sub add_url_and_name {
+sub add_searching_url_and_name {
     my $self = shift;
     my $name = shift;
 
-    $self->url($self->url . $name);
+    $self->searching_url($self->searching_url . $name);
 }
 
 sub format_json_from_github_apt {
@@ -34,8 +42,7 @@ sub format_json_from_github_apt {
     my $searching_url = shift;
 
     my $json_plane_text = `curl $searching_url`;
-    my $json_data decode_json($json_plane_text);
-    $self->json_data($json_data);
+    decode_json($json_plane_text);
 }
 
 1;
